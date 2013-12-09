4f82dc1e532aa40881bfd1764cdd5774fabcc67a
Marcelo Dominguez <marcelo@wyeworks.com>
2013-12-09 20:28:46 -0200

    Added database.yml

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0.beta1'
 
-gem 'sqlite3'
+gem 'mysql2'
 gem 'devise'
 
 # Gems used only for assets and not required
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+marce = User.create!(
+  email: "marcelo@wyeworks.com",
+  password: "12341234",
+  admin: false
+)
+leo = User.create!(
+  email: "leo@wyeworks.com",
+  password: "12341234",
+  admin: false
+)
+admin = User.create!(
+  email: "admin@wyeworks.com",
+  password: "12341234",
+  admin: true
+)
+
+Order.create!(
+  description: "Orden",
+  total: 200,
+  user_id: admin.id
+)
+
+Order.create!(
+  description: "Orden",
+  total: 50,
+  user_id: marce.id
+)
+Order.create!(
+  description: "Orden",
+  total: 100,
+  user_id: leo.id
+)
