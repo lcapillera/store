d3f79cae5337d4f5ec7ddf3d841759848ed38e85
Leonardo Capillera <leonardo@wyeworks.com>
2013-12-08 22:43:40 -0200

    Add auth to order controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Store::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root to: 'welcome#index'
-  #root to: "home#index"
+  root to: "orders#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
